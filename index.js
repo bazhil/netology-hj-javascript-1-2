e947fd12c521865e91b2f9f68f63aed9f4c74930
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-09-23 21:44:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-'use strict'
-
-var photoNike = [
-  'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-jump.png',
-  'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-on-foot.png',
-  'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-playground.png',
-  'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-top-view.png',
-  'https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax.png'
-];
-
-var slider = document.getElementById('slider');
-
-slider.src = photoNike[0];
-
-let i = 0;
-let timer = setInterval(function(){
-  if (i > photoNike.length - 1){
-    i = 0;
-  }
-  slider.src = photoNike[i];
-  i++;
-}, 5000);
+'use strict';
+function handleClick() {
+  openMail();
+}
+const button = document.getElementById('open');
+button.onclick = handleClick;
